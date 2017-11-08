cd268b11907953b746d8d5fad69f56034db05f21
Jamie Ellis <Jamie.Ellis@softwire.com>
2017-11-08 12:22:02 +0000

    Got balance check and csv parsing working.

--- index.js
+++ index.js
@@ -2,22 +2,70 @@ let readlineSync = require('readline-sync');
 let moment = require('moment');
 let fastCsv = require('fast-csv');
 
-
-
 class Transaction {
     constructor(date, from, to, narrative, amount) {
-        this.date = date;
-        this.from = from;
-        this.to = to;
-        this.narrative = narrative;
-        this.amount = amount;
+        this.Date = date;
+        this.From = from;
+        this.To = to;
+        this.Narrative = narrative;
+        this.Amount = amount;
+    }
+
+    display() {
+        console.log(`Transaction: ${this.Amount} transferred from ${this.From} to ${this.To} on ${this.Date.format('Do MMMM YYYY')} (${this.Narrative}).`);
     }
 }
 
-function pullCSVTransactions() {
-    // Reads CSV transaction data into an array of Transaction objects.
-    let readStream = fastCsv.createReadStream('Transactions2014.csv');
-    csv.fromStream(readStream).on('data', (data) => {console.log(data);});
+class User {
+    constructor(name, balance) {
+        this.Name = name;
+        this.Balance = balance
+    }
+
+    display() {
+        console.log(`User: ${this.Name} has ${this.Balance.toFixed(2)}`);
+    }
 }
 
-pullCSVTransactions();
+function parseTransactionsCSV(terminationFunction) {
+    let transactions = [];
+    fastCsv.fromPath('Transactions2014.csv', {headers: true})
+        .transform((entry) => {
+            return new Transaction(moment(entry.Date, 'DD-MM-YYYY'), entry.From, entry.To, entry.Narrative, parseFloat(entry.Amount));
+        })
+        .on('data', (transaction) => {
+           transactions.push(transaction);
+        })
+        .on('end', () => {
+            terminationFunction(transactions)
+        });
+}
+
+function evaluateBalance(transactions) {
+    let users = [];
+    for (transactionId = 0; transactionId < transactions.length; ++transactionId) {
+        let transaction = transactions[transactionId];
+
+        let fromUser = users.find((user) => { return user.Name === transaction.From; });
+        let toUser = users.find((user) => { return user.Name === transaction.To; });
+        if (fromUser === undefined) {
+            users.push(new User(transaction.From, 0));
+            fromUser = users[users.length - 1];
+        }
+        if (toUser === undefined) {
+            users.push(new User(transaction.To, 0));
+            toUser = users[users.length - 1];
+        }
+
+        fromUser.Balance -= transaction.Amount;
+        toUser.Balance += transaction.Amount;
+
+        transaction.display();
+    }
+
+    for (userId = 0; userId < users.length; ++userId) {
+        users[userId].display();
+    }
+}
+
+parseTransactionsCSV(evaluateBalance);
